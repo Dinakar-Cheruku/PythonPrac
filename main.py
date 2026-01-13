3ce4aa372d300798ffe703b3dc1bf23cd9ebdb55
Dinakar Cheruku <dinakarcheruku2002@gmail.com>
2026-01-13 16:49:23 -0500

    made some changes, good learnings.

--- main.py
+++ main.py
@@ -1,1 +1,21 @@
-print("lorem ipsum bismillah abdhullah")
+#print("lorem ipsum bismillah abdhullah")
+
+# for c in range(2,30,4):
+#     print(c)
+# else:
+#     print("done")
+
+import time
+def fib(n):
+    a, b = 0, 1
+    for _ in range(n):
+        print(a)
+        a, b = b, a + b
+
+s = time.time()
+fib(1000)
+e = time.time()
+
+print(e - s, "seconds")
+
+
