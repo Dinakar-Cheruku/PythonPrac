d1536fbc5caf6107f69aac7ebc1db880f3e55a50
Dinakar Cheruku <dinakarcheruku2002@gmail.com>
2026-01-13 17:06:06 -0500

    if ands elifs

--- main.py
+++ main.py
@@ -20,9 +20,20 @@
 
 #Fun learnings
 
-name = input("Enter you fav char name:")
-age = int(input("Enter your age:"))
-ageH = int(input("Enter ur fav chars age:"))
+# name = input("Enter you fav char name:")
+# age = int(input("Enter your age:"))
+# ageH = int(input("Enter ur fav chars age:"))
+#
+# print(f"Hehehe, you are ",age+ageH," times a bigger idiot than",name)
 
-print(f"Hehehe, you are ",age+ageH," times a bigger idiot than",name)
+x, y = 7,15
+# if (x>0 and y<20):print(x,y)
+# if (x>0 or y>20):print(x,y)
+
+# if x>0:print("x>0")
+# if x<0:print("x<0")
+# if y<20:print("y<20")
+# else:print("y>20")
+
+# print("x>0" if x > 0 else "", "y<20" if y < 20 else "")
 
